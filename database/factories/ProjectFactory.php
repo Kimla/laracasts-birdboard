a44b818ab03ac5958b008e8cf6722368f760c92b
Kim Larsson <kim@bravissimo.se>
2019-02-21 11:51:33 +0100

    'EP5'

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,5 +6,8 @@ $factory->define(App\Project::class, function (Faker $faker) {
     return [
         'title' => $this->faker->sentence,
         'description' => $this->faker->paragraph,
+        'owner_id' => function() {
+            return factory(App\User::class)->create()->id;
+        }
     ];
 });
