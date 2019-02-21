2d811e170269f83056f9e1bcb7748ab6942aeaf3
Kim Larsson <kim@bravissimo.se>
2019-02-21 10:48:20 +0100

    'EP3'

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -28,4 +28,26 @@ class ProjectsTest extends TestCase
         $this->get('/projects')
             ->assertSee($attributes['title']);
     }
+
+    /** @test */
+    public function a_project_required_a_title()
+    {
+        $attributes = factory('App\Project')->raw([
+            'title' => '',
+        ]);
+
+        $this->post('/projects', $attributes)
+            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_required_a_description()
+    {
+        $attributes = factory('App\Project')->raw([
+            'description' => '',
+        ]);
+
+        $this->post('/projects', $attributes)
+            ->assertSessionHasErrors('description');
+    }
 }
